d2c4dc6af3f917345b29d0b4e48c4e38c9a5fa92
Nihalkarimbil <muhammednihalk4@gmail.com>
2025-07-07 09:41:58 +0530

    some fixes

--- client/src/lib/utils/Axios.js
+++ client/src/lib/utils/Axios.js
@@ -1,8 +1,8 @@
 import axios from "axios"
 
 const axiosInstance= axios.create({
-    //baseURL: "https://purity-manager.onrender.com/api",
-    baseURL: "http://localhost:3000/api",
+    baseURL: "https://purity-manager.onrender.com/api",
+    //baseURL: "http://localhost:3000/api",
     withCredentials:true
 })
 
